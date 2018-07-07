89e98c664a6325c98f3890299993f6eae0473bdb
Jeffrey Wang <j@mywikis.com>
2018-07-07 17:43:14 -0500

    Adding stats

--- index.js
+++ index.js
@@ -96,6 +96,17 @@ function randomColor() {
     return color;
 }
 
+// from Stack Overflow https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript/901144#901144
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 function compileChart( result ) {
     var {term, subj, num, sect, prof, grades} = result;
     var total = Object.values(grades).reduce((a, b) => a+parseInt(b), 0);
@@ -151,4 +162,11 @@ function compileChart( result ) {
           })
         }]
     });
+    var totalgrades = 0;
+    Object.values(grades).forEach((num) => {
+      totalgrades += parseInt(num);
+    });
+    $('#stats').text('Total grades: ' + totalgrades);
+    $('#sharelink').val(window.location.href + '?term=' + encodeURIComponent(term) + '&subj=' + encodeURIComponent(subj) + '&num=' + encodeURIComponent(num) + '&sect=' + encodeURIComponent(sect) + '&prof=' + encodeURIComponent(prof) + '&grades=' + encodeURIComponent(btoa(JSON.stringify(grades))));
+    $('#share').show();
 }
