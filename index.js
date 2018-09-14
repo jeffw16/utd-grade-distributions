e5a6058804a3184a05b17944f8c1d82a67920264
Jeffrey Wang <j@mywikis.com>
2018-09-14 17:43:41 -0500

    Added fixed JSON link

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://cdn.rawgit.com/jeffw16/utd-grade-distributions/2c0c616dfaba7d4bebe06f97d9ef1b212e66de84/static/complete.json', true);
+    xhr.open('GET', 'https://cdn.rawgit.com/jeffw16/utd-grade-distributions/09c9b71584da99d66cb8acc09d58d0c50415ecf4/static/complete.json', true);
     // xhr.open('GET', 'https://www.google.com/blank.html', true);
     xhr.responseType = 'json';
     // console.log('started loading db');
